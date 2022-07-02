3dbb6ae893c2ff979c615ca1d2a75dd472f561d8
Bastech12 <105978264+Bastech12@users.noreply.github.com>
2022-07-02 13:10:50 -0400

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,14 +2,14 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
-       sh	"$MHD/bin/mvn clean package"
+       sh	"mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh	"$MHD/bin/mvn sonar:sonar"
+       sh	"mvn sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
-       sh	"$MHD/bin/mvn deploy"
+       sh	"mvn deploy"
      }
 }
