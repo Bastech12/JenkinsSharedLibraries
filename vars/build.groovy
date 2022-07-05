099afee37290ffaad25f1d1d9b7a298371377772
Bastech12 <105978264+Bastech12@users.noreply.github.com>
2022-07-04 20:32:32 -0400

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,15 +2,15 @@ def call(String mavenGoal){
   
   if ("${mavenGoal}" == "Clean")
      {
-       sh "/home/ec2-user/ugo-node9/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean"
+       sh "/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean"
      }
   else if ("${mavenGoal}" == "Compile")
      {
-       sh "/home/ec2-user/ugo-node9/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean compile"
+       sh "/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean compile"
      }
   else if ("${mavenGoal}" == "Test")
      {
-       sh "/home/ec2-user/ugo-node9/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean test"
+       sh "/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven3.8.4/bin/mvn clean test"
      }
    else if ("${mavenGoal}" == "Package")
       {
